ac7169e0f405d64a0c9d6965f5e1d02f3fd91f21
Xorg <xorgbreaker@gmail.com>
2015-07-08 15:52:41 +0200

    Improve pre-commit hook

--- scripts/update_hook.sh
+++ scripts/update_hook.sh
@@ -6,13 +6,14 @@
 #	A Git hook to help to update a package.
 ############################################################################
 
-[[ $# == 0 ]] && echo -e "Package name is missing.\n" && exit 1
-
-cd $(find . -name $1)
+[[ $# != 0 ]] && cd $(find . -name $1)
 
 ls -1 > /tmp/fileslist
-[[ $SUM != 0 ]] && updpkgsums
+[[ $SUM != 0 ]] && echo -e "\033[36mUpdate sums\033[0m" && updpkgsums
 ls -1 >> /tmp/fileslist
+
+echo -e "\033[36mRemove temporary files\033[0m"
 sort /tmp/fileslist | uniq -u | xargs rm
 
+echo -e "\033[36mUpdate .SRCINFO file\033[0m"
 mksrcinfo
