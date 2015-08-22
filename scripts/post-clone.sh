4f9bbfcca13d725b9b565f45155c4699b9478e02
Xorg <xorgbreaker@gmail.com>
2015-08-22 17:39:12 +0200

    Add a script to run after a clone

---
+++ scripts/post-clone.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+############################################################################
+#			post-clone
+#
+#	Apply hooks to submodule and branch HEAD to master.
+############################################################################
+
+rep="$(git rev-parse --show-toplevel)"
+
+echo -e "\033[36mAdd hooks for submodules...\033[0m"
+for file in $(ls "$rep/.git/modules"); do
+	ln -vs "$rep/scripts/commit_hook.sh" "$rep/.git/modules/$file/hooks/post-commit"
+	ln -vs "$rep/scripts/update_hook.sh" "$rep/.git/modules/$file/hooks/pre-commit"
+done
+
+echo -e "\033[36mCheckout master for submodules...\033[0m"
+git submodule foreach git checkout master
