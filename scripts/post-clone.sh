d1935de37954a566fe479c27ccf820f6378becf7
Xorg <xorgbreaker@gmail.com>
2015-08-28 11:40:25 +0200

    Some fix in scripts with gnustep-clang-svn packages

--- scripts/post-clone.sh
+++ scripts/post-clone.sh
@@ -9,7 +9,7 @@
 rep="$(git rev-parse --show-toplevel)"
 
 echo -e "\033[36mAdd hooks for submodules...\033[0m"
-for file in $(ls "$rep/.git/modules"); do
+for file in $(git submodule | awk '{ print $2 }'); do
 	ln -vs "$rep/scripts/commit_hook.sh" "$rep/.git/modules/$file/hooks/post-commit"
 	ln -vs "$rep/scripts/update_hook.sh" "$rep/.git/modules/$file/hooks/pre-commit"
 done
