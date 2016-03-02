f85d6f131f4e3247b2b62bc115ef532f07cec8b2
Xorg <xorgbreaker@gmail.com>
2016-03-02 19:55:12 +0100

    Rewrite post_clone script

---
+++ scripts/post_clone.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+############################################################################
+#			post_clone
+#
+#	Apply hooks to current Git repository.
+############################################################################
+
+GIT_ROOT="$(git rev-parse --show-toplevel)"
+
+cd "$GIT_ROOT/.git/hooks"
+echo -e "\033[36mSetting Git hooks...\033[0m"
+ln -vs "../../scripts/pre_commit.hook.sh"  "pre-commit"
+ln -vs "../../scripts/post_commit.hook.sh" "post-commit"
