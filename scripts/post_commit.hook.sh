5eeb69702136eb7e48bfad0d3e6fd290c25e58c5
Xorg <xorgbreaker@gmail.com>
2016-03-04 11:02:58 +0100

    Hooks: fixes

--- scripts/post_commit.hook.sh
+++ scripts/post_commit.hook.sh
@@ -7,12 +7,12 @@
 ############################################################################
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
-PACKAGE=$(dirname `git log --pretty=format: --name-only -n1`)
+PACKAGE=$(dirname `git log --pretty=format: --name-only -n1 | head -n1`)
 SHA_1=$(git rev-parse HEAD)
 COMMIT_MSG=$(git log -1 --pretty=%B)
 TMP_DIR="/tmp/aur-repos"
 
-( [[ ! -f "$GIT_ROOT/$PACKAGE/PKGBUILD" ]] || (( $SKIP_POST )) ) && exit 0
+( [[ ! -f "$GIT_ROOT/$PACKAGE/PKGBUILD" ]] || [[ $SKIP_POST == 1 ]] ) && exit 0
 
 if [[ ! -d "$TMP_DIR/$PACKAGE" ]]; then
 	git clone ssh+git://aur@aur.archlinux.org/$PACKAGE.git "$TMP_DIR/$PACKAGE"
@@ -25,5 +25,5 @@ git --git-dir="$GIT_ROOT/.git" \
 
 cd "$GIT_ROOT/"
 export SKIP_POST=1
-git commit --amend -m "$PACKAGE: $COMMIT_MSG"
+git commit --no-verify --amend -m "$PACKAGE: $COMMIT_MSG"
 unset SKIP_POST
