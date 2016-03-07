8112bba9fa69fcb63dcdda3f5d77ebf89cb5a9fd
Xorg <xorgbreaker@gmail.com>
2016-03-07 13:47:41 +0100

    Hook: fix pre-push

--- scripts/pre_push.hook.sh
+++ scripts/pre_push.hook.sh
@@ -9,5 +9,7 @@
 PACKAGE=$(dirname `git log --pretty=format: --name-only -n1 | head -n1`)
 TMP_DIR="/tmp/aur-repos"
 
+[[ ! -f "$TMP_DIR/$PACKAGE/PKGBUILD" ]] && exit 0
+
 cd "$TMP_DIR/$PACKAGE"
 git push
