92e87a860acac75895d11c00c9a88b1fa0104ed5
Xorg <xorgbreaker@gmail.com>
2018-10-19 14:16:00 +0200

    Scripts: Fix pre-push hook

--- scripts/pre-push.hook.sh
+++ scripts/pre-push.hook.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for path in $(git diff --name-only --cached --diff-filter=AM); do
+for path in $(git show --name-only); do
     if [[ "${path}" =~ .*/PKGBUILD$ ]]; then
+        echo -e "\e[01;32m *** Pushing ${path%/PKGBUILD} to AUR ***\e[00m"
         aurpublish ${path%PKGBUILD}
     fi
 done
